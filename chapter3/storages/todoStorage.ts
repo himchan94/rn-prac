18fa542191e909c4b56cc77d7cc6948b459edea6
himchan94 <himchan9494@gmail.com>
2022-10-23 02:23:38 +0900

    feat: Create storage managing function

---
+++ chapter3/storages/todoStorage.ts
@@ -0,0 +1,33 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const key = 'todos';
+
+const todosStorage = {
+  async get() {
+    try {
+      const rawTodos = await AsyncStorage.getItem(key);
+
+      if (!rawTodos) {
+        return;
+      }
+
+      const savedTodos = JSON.parse(rawTodos);
+      return savedTodos;
+    } catch (e) {
+      throw new Error('Failed to load todos');
+    }
+  },
+  async set(data: Todo[]) {
+    try {
+      if (!data) {
+        return;
+      }
+
+      await AsyncStorage.setItem(key, JSON.stringify(data));
+    } catch (e) {
+      throw new Error('Failed to save todos');
+    }
+  },
+};
+
+export default todosStorage;
