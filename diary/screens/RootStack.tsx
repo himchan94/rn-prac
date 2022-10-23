5e4e675e7cc7ffe138509ed0ac9048b9c25853ac
himchan94 <himchan9494@gmail.com>
2022-10-23 22:17:14 +0900

    feat: Create basic components and routes

---
+++ diary/screens/RootStack.tsx
@@ -0,0 +1,24 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import MainTab from './MainTab';
+import WriteScreen from './WriteScreen';
+
+const Stack = createNativeStackNavigator();
+
+const RootStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="MainTab"
+        component={MainTab}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen name="write" component={WriteScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default RootStack;
+
+const styles = StyleSheet.create({});
