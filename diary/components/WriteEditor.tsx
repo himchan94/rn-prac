7a8c4f4dc9e75dbc212fac2e241ca69145e23e9f
himchan94 <himchan9494@gmail.com>
2022-11-03 23:59:09 +0900

    feat: Apply ref

---
+++ diary/components/WriteEditor.tsx
@@ -0,0 +1,61 @@
+import {StyleSheet, Text, TextInput, View} from 'react-native';
+import React, {useRef} from 'react';
+
+interface WriteEditorProps {
+  title: string;
+  body: string;
+  onChangeTitle: () => void;
+  onChangeBody: () => void;
+}
+
+const WriteEditor = ({
+  title,
+  body,
+  onChangeTitle,
+  onChangeBody,
+}: WriteEditorProps) => {
+  const bodyRef = useRef();
+
+  return (
+    <View style={styles.block}>
+      <TextInput
+        placeholder="제목을 입력하세요"
+        style={styles.titleInput}
+        returnKeyType="next"
+        onChangeText={onChangeTitle}
+        value={title}
+        onSubmitEditing={() => {
+          bodyRef.current.focus();
+        }}
+      />
+      <TextInput
+        placeholder="당신의 오늘을 기록하세요"
+        style={styles.bodyInput}
+        multiline
+        textAlignVertical="top"
+        onChange={onChangeBody}
+        value={body}
+        ref={bodyRef}
+      />
+    </View>
+  );
+};
+
+export default WriteEditor;
+
+const styles = StyleSheet.create({
+  block: {flex: 1, padding: 16},
+  titleInput: {
+    paddingVertical: 0,
+    fontSize: 18,
+    marginBottom: 16,
+    color: '#263238',
+    fontWeight: 'bold',
+  },
+  bodyInput: {
+    flex: 1,
+    fontSize: 16,
+    paddingVertical: 0,
+    color: '#263228',
+  },
+});
